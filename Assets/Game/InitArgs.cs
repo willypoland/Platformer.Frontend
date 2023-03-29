aa9bab510b352512eed9dfff543937d532a312f5
willy <willypoland0@gmail.com>
2023-03-29 19:32:20 +0300

    add ui

---
+++ Assets/Game/InitArgs.cs
@@ -0,0 +1,13 @@
+﻿using System.Net;
+
+
+namespace Game
+{
+    public struct StartArgs
+    {
+        public bool IsLocal;
+        public ushort LocalPort;
+        public IPAddress Ip;
+        public ushort RemotePort;
+    }
+}
