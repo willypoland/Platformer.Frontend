76a31802c9b7e0f96b641975399fd5678ec96ef1
willypoland <willypoland0@gmail.com>
2023-04-02 14:35:01 +0300

    frame animation tests

---
+++ Assets/Game/Tests/FramedAnimationClip.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+
+namespace Game.Tests
+{
+    [CreateAssetMenu(menuName = "Game/Test/FramedAnimationClip")]
+    class FramedAnimationClip : ScriptableObject
+    {
+        [SerializeField] private Sprite[] _sprites;
+
+        public Sprite Get(int i) => _sprites[Mathf.Abs(i) % _sprites.Length];
+
+        public int Count => _sprites.Length;
+
+        public Sprite GetLerp(int i, int last) => Get(Mathf.RoundToInt(i / (float) last * Count));
+    }
+}
---
+++ Assets/Game/Tests/StatesStream.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+
+namespace Game.Tests
+{
+    // attack: 6
+    // jump: 12
+    // landing: 0
+    enum AnimState
+    {
+        Idle, Attack, Jump, Falling, Landing, Run, 
+    }
+
+
+    struct AnimFrame
+    {
+        public readonly AnimState State;
+        public readonly int StateFrame;
+        public readonly int TotalFrame;
+
+        public AnimFrame(AnimState state, int stateFrame, int totalFrame)
+        {
+            State = state;
+            StateFrame = stateFrame;
+            TotalFrame = totalFrame;
+        }
+
+        public override string ToString() => $"{State}.{StateFrame} : {TotalFrame}";
+    }
+    
+    class StatesStream : IEnumerable<AnimFrame>
+    {
+        public readonly List<AnimState> _frames = new();
+        
+        public StatesStream AddIdle(int frames) => AddAnimState(AnimState.Idle, frames);
+        
+        public StatesStream AddRun(int frames) => AddAnimState(AnimState.Run, frames);
+        
+        public StatesStream AddLanding() => AddAnimState(AnimState.Landing, 1);
+        
+        public StatesStream AddFalling(int frames) => AddAnimState(AnimState.Falling, frames);
+        
+        public StatesStream AddJump() => AddAnimState(AnimState.Jump, 12);
+        
+        public StatesStream AddAttack() => AddAnimState(AnimState.Attack, 6);
+
+        private StatesStream AddAnimState(AnimState state, int frames)
+        {
+            for (int i = 0; i < frames; i++)
+                _frames.Add(state);
+
+            return this;
+        }
+
+        public IEnumerator<AnimFrame> GetEnumerator()
+        {
+            if (_frames.Count == 0)
+                yield break;
+
+            AnimState last = _frames[0];
+            int total = 0;
+            int current = 0;
+            
+            while (true)
+            {
+                yield return new AnimFrame(last, current, total);
+                total += 1;
+                current += 1;
+
+                AnimState next = _frames[total % _frames.Count];
+
+                if (next != last)
+                {
+                    current = 0;
+                    last = next;
+                }
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
